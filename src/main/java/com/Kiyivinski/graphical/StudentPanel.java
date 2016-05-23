1d09720c28ddee49e380833d538894fd302d51bc
Timur Kiyivinski <deathseclipse@gmail.com>
2016-05-23 21:37:32 +0800

    [WIP] Student panel, added scrollable table

--- src/main/java/com/Kiyivinski/graphical/StudentPanel.java
+++ src/main/java/com/Kiyivinski/graphical/StudentPanel.java
@@ -3,21 +3,31 @@ package com.Kiyivinski.graphical;
 import com.Kiyivinski.models.Student;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.util.List;
 
 public class StudentPanel extends JPanel {
     StudentPanel() {
+        this.setBorder(new EmptyBorder(5, 5, 5, 5));
+
         GridLayout layout = new GridLayout(1, 2, 25, 25);
         this.setLayout(layout);
 
         StudentTable studentPane = new StudentTable();
+        studentPane.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+
+        JScrollPane panelLeft = new JScrollPane(studentPane);
+        JPanel panelRight = new JPanel();
 
         List<Student> students = Student.all();
         for (Student s: students) {
             studentPane.addRow(s);
+            studentPane.addRow(s);
         }
 
-        this.add(studentPane);
+
+        this.add(panelLeft);
+        this.add(panelRight);
     }
 }
